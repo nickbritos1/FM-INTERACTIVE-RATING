d6e8d432fcddcf7fdcca0938a796a8ebb03a7335
Nica Britos <nicanorbritos@gmail.com>
2023-08-29 12:44:05 -0300

    agregado js

---
+++ app.js
@@ -0,0 +1,15 @@
+const boton = document.querySelectorAll(".botonPun")
+
+
+
+boton.forEach(boton => {
+    boton.addEventListener("click", () => {
+        botonElegido(boton);
+    });
+});
+
+
+
+function botonElegido(boton) {
+    let valor = boton.getAttribute("value");
+}
